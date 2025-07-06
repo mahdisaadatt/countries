01b40838cdf425a4265c6cdf585927794247dc7a
Mahdi <mahdi.developer1@gmail.com>
2025-07-06 23:25:30 +0330

    testing proxy

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -79,7 +79,7 @@ const CountriesList = ({
     try {
       if (!localStorage.getItem('countries')) {
         const res = await axios.get(
-          '/countries?fields=name,flags,alpha2Code,alpha3Code,population,region,capital,nativeName,subregion,topLevelDomain,currencies,languages,borders'
+          '/api/countries?fields=name,flags,alpha2Code,alpha3Code,population,region,capital,nativeName,subregion,topLevelDomain,currencies,languages,borders'
         );
         const data = await res.data;
         localStorage.setItem('countries', JSON.stringify(data));
